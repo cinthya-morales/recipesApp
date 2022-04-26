fc4586e1a8c9dfbb1b45887253752b52c4c989f5
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-04-26 17:49:47 -0300

    Rotas configuradas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Routes from './routes';
 import ContextProvider from './context/contexProvider';
 import Login from './components/Login';
 
 function App() {
   return (
     <ContextProvider>
-      <Login />
+      <Routes>
+        <Login />
+      </Routes>
     </ContextProvider>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -43,7 +43,6 @@ function Login() {
       >
         Enter
       </button>
-
     </form>
   );
 }
