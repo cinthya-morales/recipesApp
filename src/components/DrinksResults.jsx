f0bc2f6d8c926378cae6872132480484c139123e
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-05-02 18:42:56 -0300

    Requisitos até o 32 terminados

--- src/components/DrinksResults.jsx
+++ src/components/DrinksResults.jsx
@@ -1,22 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import SearchContext from '../context/searchContext';
-import { fetchDrinksByCategory } from '../services/fetchAPI';
+import { fetchDrinksByCategory, getDrinksRecipes } from '../services/fetchAPI';
 
 function DrinksResults() {
   const { drinksList, setDrinksList,
     drinksCategoryList } = useContext(SearchContext);
 
+  const [filtered, setFiltered] = useState('');
+
   const maxNumber = 12;
   const MAX_CATEGORY = 5;
 
   const filterCategory = async (category) => {
-    const data = await fetchDrinksByCategory(category);
-    setDrinksList(data);
+    if (filtered !== category && category !== 'All') {
+      const drinksByCategoryData = await fetchDrinksByCategory(category);
+      setDrinksList(drinksByCategoryData);
+      setFiltered(category);
+    } else {
+      const allDrinks = await getDrinksRecipes();
+      setDrinksList(allDrinks);
+      setFiltered('');
+    }
   };
 
   return (
     <main>
       <section>
+        <button
+          type="button"
+          data-testid="All-category-filter"
+          value="All"
+          onClick={ (event) => filterCategory(event.target.value) }
+        >
+          All
+        </button>
         {drinksCategoryList
           .slice(0, MAX_CATEGORY)
           .map((elem, index) => (
@@ -34,11 +52,17 @@ function DrinksResults() {
 
       {drinksList.length > 0 && drinksList
         .filter((_e, index) => index < maxNumber)
-        .map(({ strDrink, strDrinkThumb }, index) => (
-          <div key={ index } data-testid={ `${index}-recipe-card` }>
-            <p data-testid={ `${index}-card-name` }>{strDrink}</p>
-            <img src={ strDrinkThumb } alt="recipe" data-testid={ `${index}-card-img` } />
-          </div>
+        .map(({ strDrink, strDrinkThumb, idDrink }, index) => (
+          <Link push to={ `/drinks/${idDrink}` } key={ index }>
+            <div data-testid={ `${index}-recipe-card` }>
+              <p data-testid={ `${index}-card-name` }>{strDrink}</p>
+              <img
+                src={ strDrinkThumb }
+                alt="recipe"
+                data-testid={ `${index}-card-img` }
+              />
+            </div>
+          </Link>
         ))}
     </main>
   );
--- src/components/FoodsResults.jsx
+++ src/components/FoodsResults.jsx
@@ -1,22 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import SearchContext from '../context/searchContext';
-import { fetchFoodsByCategory } from '../services/fetchAPI';
+import { getFoodRecipes, fetchFoodsByCategory } from '../services/fetchAPI';
 
 function FoodsResults() {
   const { foodsList, setFoodsList,
     foodsCategoryList } = useContext(SearchContext);
 
+  const [filtered, setFiltered] = useState('');
+
   const maxNumber = 12;
   const MAX_CATEGORY = 5;
 
   const filterCategory = async (category) => {
-    const data = await fetchFoodsByCategory(category);
-    setFoodsList(data);
+    if (filtered !== category && category !== 'All') {
+      const foodsByCategoryData = await fetchFoodsByCategory(category);
+      setFoodsList(foodsByCategoryData);
+      setFiltered(category);
+    } else {
+      const allFoods = await getFoodRecipes();
+      setFoodsList(allFoods);
+      setFiltered('');
+    }
   };
 
   return (
     <main>
       <section>
+        <button
+          type="button"
+          data-testid="All-category-filter"
+          value="All"
+          onClick={ (event) => filterCategory(event.target.value) }
+        >
+          All
+        </button>
         {foodsCategoryList
           .slice(0, MAX_CATEGORY)
           .map((elem, index) => (
@@ -34,11 +52,17 @@ function FoodsResults() {
 
       {foodsList.length > 0 && foodsList
         .filter((_e, index) => index < maxNumber)
-        .map(({ strMeal, strMealThumb }, index) => (
-          <div key={ index } data-testid={ `${index}-recipe-card` }>
-            <p data-testid={ `${index}-card-name` }>{strMeal}</p>
-            <img src={ strMealThumb } alt="recipe" data-testid={ `${index}-card-img` } />
-          </div>
+        .map(({ strMeal, strMealThumb, idMeal }, index) => (
+          <Link push to={ `/foods/${idMeal}` } key={ index }>
+            <div data-testid={ `${index}-recipe-card` }>
+              <p data-testid={ `${index}-card-name` }>{strMeal}</p>
+              <img
+                src={ strMealThumb }
+                alt="recipe"
+                data-testid={ `${index}-card-img` }
+              />
+            </div>
+          </Link>
         ))}
     </main>
   );
