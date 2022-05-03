9b35789a2140113fc68975702a1f32c8a51dc9b6
vitorbelarmino <vitor.belarmino@hotmail.com>
2022-05-02 23:03:44 -0300

    tela de perfil finalizada

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-function Profile() {
+function Profile({ history }) {
+  const user = JSON.parse(localStorage.getItem('user')).email;
+  function clearLocalStorage() {
+    localStorage.clear();
+    history.push('/');
+  }
   return (
     <div>
       <Header title="Profile" />
+      <h2 data-testid="profile-email">{user}</h2>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ clearLocalStorage }
+        >
+          Logout
+
+        </button>
+      </div>
       <Footer />
     </div>
   );
 }
+Profile.propTypes = {
+  history: propTypes.shape({
+    push: propTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Profile;
