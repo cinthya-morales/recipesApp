af856852a900a68b82e867c089e2c10e1efb12e7
Cinthya Morales <cinthya.morales@outlook.com>
2022-05-03 11:19:45 -0300

    requisitos 67 ao 73

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Explorer from '../components/Explorer';
 
 function Explore() {
   return (
     <div>
       <Header title="Explore" />
+      <Explorer />
       <Footer />
     </div>
   );
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ExploreFoodsComponent from '../components/ExploreFoodsComponent';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,7 @@ function ExploreFoods() {
   return (
     <div>
       <Header title="Explore Foods" />
+      <ExploreFoodsComponent />
       <Footer />
     </div>
   );
